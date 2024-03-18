0d76904add4777b364a49990c34382e829cf60b3
FANGPLAY SIRIRAK <40180085+fangplay@users.noreply.github.com>
2024-03-18 20:40:26 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charset="utf-8" />
-{/*         <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+{/*         <meta charset="utf-8" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
           rel="stylesheet"

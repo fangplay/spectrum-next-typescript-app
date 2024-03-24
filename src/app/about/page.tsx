9e0e3f8fb7ef90263cc7346620a3a9a9e98557a9
FANGPLAY SIRIRAK <40180085+fangplay@users.noreply.github.com>
2024-03-24 16:53:36 +0700

    remove beta test banner into real version

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import profilepic from "./images/profile-2024.jpg";
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           This web that you viewing&nbsp;
           <code className="font-mono font-bold">is beta test.</code>
@@ -25,7 +25,7 @@ export default function About() {
             />
           </p>
         </div>
-      </div>
+      </div> */}
       <div className="relative flex place-items-center">
         <h1>About FANGPLAY</h1>
       </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Contaxt() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           This web that you viewing&nbsp;
           <code className="font-mono font-bold">is beta test.</code>
@@ -24,7 +24,7 @@ export default function Contaxt() {
             />
           </p>
         </div>
-      </div>
+      </div> */}
       <div className="relative flex place-items-center">
         <h1>Contact</h1>
       </div>
